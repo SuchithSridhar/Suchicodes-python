ece70d946ff9f508b9c8215d06ec71466ec77b57
Suchith Sridhar Khajjayam <suchithsridhar@gmail.com>
2022-01-18 19:44:10 -0400

    Changed secret key of server and started using env vars rather than hard code.

--- suchiblog/config.py
+++ suchiblog/config.py
@@ -1,6 +1,7 @@
+import os
+
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = "jalkdsfjalskdfjalkwsdjflkaj"
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     RESOURCES_DIR = 'data'
     PROJECTS_UPLOAD_FOLDER = "./suchiblog/static/uploaded-data"
