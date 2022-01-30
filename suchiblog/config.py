9430828a16621ea27d74d5da2e591d7d8ef299d4
Suchith Sridhar Khajjayam <suchithsridhar@gmail.com>
2022-01-30 10:14:26 -0400

    Added track modifications config for sql alchemy.

--- suchiblog/config.py
+++ suchiblog/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     RESOURCES_DIR = 'data'
     PROJECTS_UPLOAD_FOLDER = './suchiblog/static/uploaded-data'
     NOTIFY_KEY = os.environ.get('FLASK_NOTIFY_KEY')
