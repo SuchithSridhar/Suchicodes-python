2d504b0a791fec0f1978baa96513c0a0aaf9e8a2
Suchith Sridhar <suchithsridhar@gmail.com>
2022-05-17 01:31:41 +0000

    updating wsgi file to change log location

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
 from suchiblog import create_app
+import logging
 
 app = create_app()
 
 if __name__ == '__main__':
+    logger = logging.getLogger('gunicorn.error')
+    app.logger.handler.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(logging.DEBUG)
     app.run(debug=False)
