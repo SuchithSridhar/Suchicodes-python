049c5623a4c9d1b06b9b24cc76d52469cb267a78
Suchith Sridhar <suchithsridhar@gmail.com>
2022-06-07 00:02:21 -0300

    Updated index button to be more easily noticed and understood.

--- suchiblog/static/js/toggle-index.js
+++ suchiblog/static/js/toggle-index.js
@@ -1,17 +1,19 @@
 $('.arrow').on('click', function(e){
+    arrow_open = "&#8250;INDEX"
+    arrow_close = "&#8249;INDEX"
     if ($(".arrow").hasClass('open')) {
         $(".arrow").removeClass('open');
         $(".index-container").removeClass('open');
 
         $(".arrow").addClass('close');
         $(".index-container").addClass('close');
-        $(".arrow").html('&#8250;');
+        $(".arrow").html(arrow_open);
     } else {
         $(".arrow").removeClass('close');
         $(".index-container").removeClass('close');
 
         $(".arrow").addClass('open');
         $(".index-container").addClass('open');
-        $(".arrow").html('&#8249;');
+        $(".arrow").html(arrow_close);
     }
 })
