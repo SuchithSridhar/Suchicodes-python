350850b30c26dab5c28bfdb0e0a339f2ee0dce3f
Suchith Sridhar <suchithsridhar@gmail.com>
2023-01-10 22:21:30 -0400

    Api documentation and new route added.

--- suchiblog/controllers/api/routes.py
+++ suchiblog/controllers/api/routes.py
@@ -1,14 +1,14 @@
 import flask as f
-from ...models import URL_Redirection
-from ...config import Config
-from ... import db
-from ...models import Category, Blog
 from flask import jsonify
+from ...models import Category, Blog
 
 api_blueprint = f.Blueprint('api', __name__)
 
+
 @api_blueprint.route("/api/resources/categories")
 def get_categories():
+    '''Get all categories defined under resources.'''
+
     categories = Category.query.all()
     response = []
     for cat in categories:
@@ -20,10 +20,13 @@ def get_categories():
             })
     return jsonify(response)
 
+
 @api_blueprint.route("/api/resources/blogs/<category_id>")
 def get_blogs_in_category(category_id):
+    '''Api endpoint to get all blogs in a given category using its ID.'''
+
     category = Category.query.filter_by(id=int(category_id)).first()
-    if category.category == "deleted":
+    if category.category.lower() == "deleted":
         return jsonify([])
 
     blogs = Blog.query.all()
@@ -39,3 +42,34 @@ def get_blogs_in_category(category_id):
             })
 
     return jsonify(response)
+
+
+@api_blueprint.route("/api/resources/blog/<blog_id>")
+def get_blog_content(blog_id):
+    '''Get the data of a blog using its ID.'''
+
+    blog = Blog.query.filter_by(id=blog_id).first()
+    blog_category = Category.query.filter_by(uuid=blog.category).first()
+    if blog_category.category.lower() == "deleted":
+        return jsonify({})
+
+    blog_data = {
+                "id": blog.id,
+                "date": blog.date,
+                "title": blog.title,
+                "brief": blog.brief,
+                "markdown": blog.markdown,
+                "html": blog.html,
+                "category": blog_category.category
+    }
+    return jsonify(blog_data)
+
+
+@api_blueprint.route("/api/")
+def api_help():
+    '''Help information about API.'''
+
+    return f.render_template(
+        'api/help.jinja',
+        title="Api Help | Suchicodes"
+        )
