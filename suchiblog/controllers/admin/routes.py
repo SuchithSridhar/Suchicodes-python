5ab1bc90b1b6b0a0cd2b942a11fadd99b0c67ff2
Suchith Sridhar <suchith.sridhar@gmail.com>
2024-06-03 15:24:45 -0300

    going to swtich back to nginx to manage proxies

--- suchiblog/controllers/admin/routes.py
+++ suchiblog/controllers/admin/routes.py
@@ -74,38 +74,6 @@ def view_external_messages():
     )
 
 
-@admin_blueprint.route('/admin/astrax/', defaults={'path': ''})
-@admin_blueprint.route('/admin/astrax/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-@fl.login_required
-def proxy(path):
-    # Construct the URL to the proxied service
-    url = f'http://localhost:33333/{path}'
-
-    # Forward the request headers and data
-    headers = {key: value for key, value in f.request.headers if key != 'Host'}
-    data = f.request.get_data() if f.request.method in [
-        'POST', 'PUT'] else None
-
-    # Make the request to the proxied service
-    resp = requests.request(
-        method=f.request.method,
-        url=url,
-        headers=headers,
-        data=data,
-        cookies=f.request.cookies,
-        allow_redirects=False
-    )
-
-    # Create a response to send back to the client
-    excluded_headers = ['content-encoding',
-                        'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for name, value in resp.raw.headers.items()
-               if name.lower() not in excluded_headers]
-
-    response = f.Response(resp.content, resp.status_code, headers)
-    return response
-
-
 def server_checkin_callback(message: Extern_Messages):
     servers = [Config.ASTRAX_SERVER_TAG, Config.BERNUM_SERVER_TAG]
 
