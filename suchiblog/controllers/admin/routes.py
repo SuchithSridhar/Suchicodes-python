cd5723d75757184d91099ef781077ec21ccbb988
Suchith Sridhar <suchith.sridhar@gmail.com>
2024-06-03 11:30:07 -0300

    Created a reverse proxy service for suchicodes to astrax

--- suchiblog/controllers/admin/routes.py
+++ suchiblog/controllers/admin/routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import flask as f
 import flask_login as fl
+import requests
 from .admin_util import AdminUtil
 from ...models import IP_Logs, URL_Redirection, Contact, Extern_Messages
 from ...util import Util
@@ -71,6 +72,38 @@ def view_external_messages():
         current_time=str(datetime.now()),
         data=data
     )
+
+
+@admin_blueprint.route('/admin/astrax/', defaults={'path': ''})
+@admin_blueprint.route('/admin/astrax/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@fl.login_required
+def proxy(path):
+    # Construct the URL to the proxied service
+    url = f'http://localhost:33333/{path}'
+
+    # Forward the request headers and data
+    headers = {key: value for key, value in f.request.headers if key != 'Host'}
+    data = f.request.get_data() if f.request.method in [
+        'POST', 'PUT'] else None
+
+    # Make the request to the proxied service
+    resp = requests.request(
+        method=f.request.method,
+        url=url,
+        headers=headers,
+        data=data,
+        cookies=f.request.cookies,
+        allow_redirects=False
+    )
+
+    # Create a response to send back to the client
+    excluded_headers = ['content-encoding',
+                        'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for name, value in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = f.Response(resp.content, resp.status_code, headers)
+    return response
 
 
 def server_checkin_callback(message: Extern_Messages):
